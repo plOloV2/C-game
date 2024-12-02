384518669f6189ee4506e97c9bae8f1eb596fb90
pl_OloV2 <ak.dziagwa@gmail.com>
2024-12-02 19:06:11 +0100

    added mouse and keyboard inputs

--- src/game.c
+++ src/game.c
@@ -23,18 +23,8 @@ int main(int argc, char *argv[]){
     SDL_Event event;
 
     while(!game->quit){
-        while(SDL_PollEvent(&event)){
-            switch(event.type){
-                case SDL_EVENT_QUIT:
-                    game->quit = 1;
-                    break;
-
-                case SDL_EVENT_KEY_DOWN:
-                    if(event.key.key == SDLK_ESCAPE)
-                        game->quit = 1;
-                    break;
-            }
-        }
+        
+        eventHandler(game, event);
 
         SDL_SetRenderDrawColor(renderer, 0, 0xf0, 0xff, 0xff);
         SDL_RenderClear(renderer);
@@ -42,6 +32,8 @@ int main(int argc, char *argv[]){
 
         SDL_RenderPresent(renderer);
         SDL_Delay(1);
+        SDL_Log("%i\nx:%f\ny:%f\n", game->keysPressed, game->mouseX, game->mouseY);
+
     }
 
     closeGame(&window, &renderer);
--- src/game.h
+++ src/game.h
@@ -2,20 +2,6 @@
 #include "video/video.h"
 #include "logic/logic.h"
 
-struct gameState;
-
-struct gameState* initGameState();
-
-int initSDL(SDL_Window** window, SDL_Renderer** renderer);
-
-// void updateGameState(struct gameState* game);
-
-// void updateScreen(SDL_Renderer** renderer, struct gameState* game);
-
-// void uppdateAudio(struct gameState* game);
-
-SDL_Texture* loadTexture(char* texturePath, SDL_Renderer** renderer);
-
 void closeGame(SDL_Window** window, SDL_Renderer** renderer){
     SDL_Log("Game closes");
     SDL_DestroyRenderer(*renderer);
